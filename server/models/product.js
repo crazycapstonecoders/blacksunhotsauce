571f1ba7d468b85f66698d90a9af86e623c8d48d
ian038 <ianphua3@gmail.com>
2021-03-16 14:02:20 -0400

    Added firebase storage to store images and enable create product

---
+++ server/models/product.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema
+
+const productSchema = new Schema({
+    name: {
+        type: String,
+        trim: true,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    quantity: {
+        type: Number,
+        required: true
+    },
+    price: {
+        type: Number,
+        trim: true,
+        required: true
+    },
+    images: [{
+        type: String
+    }]
+}, { timestamps: true }
+)
+
+const Product = mongoose.model('Product', productSchema)
+
+module.exports = Product
