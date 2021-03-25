c5713681947b3e36f30378254729208a443e4141
jwstewart-7 <jwstewart-7@github.com>
2021-03-25 18:01:10 -0400

    Further CSS refinements and general stub of intended operation so others can start work on the cart operations

--- client/src/views/ShopPage/ShopPage.js
+++ client/src/views/ShopPage/ShopPage.js
@@ -17,6 +17,15 @@ export default function ShopPage(props) {
     // const classes = useStyles()
     const { ...rest } = props
 
+
+
+
+    //Below is the returned page structure
+    //Some form of loop will have to be integrated to process the return from DB generate the card objects
+    //on add to cart, the output would be sent to the cart object with the ID flag (hidden from user) to identify which product
+
+
+
     return (
         <div>
             <Header
@@ -31,42 +40,75 @@ export default function ShopPage(props) {
                 {...rest}
             />
             <Parallax small filter image={require("assets/img/profile-bg.jpg")} />
-            <div id="divShop" class="flex grid-constainer">
+
+            
+
+            <div id="divShop" class="flex grid-container">
                 <div class="card-Shop col-xl-3 col-lg-4 col-md-6 col-sm-12 col-xs-12">
-                    <img source=""></img>
+                    <img source="" alt="placeholder"></img>
                     <h1>Product Name</h1>
                     <p alt="product-Image">Description</p>
-                    <button type="button" action="">Add to cart</button>
-                    <input type="number"></input>
+                    <div class="flex grid-container">
+                        <form action="">
+                            <button type="submit" action="" class="col-md-8 col-xs-12">Add to cart</button>
+                            <input type="number" class="col-md-4 col-xs-12"></input>
+                            <input type="text" class="hidden" value="#ProductID"></input>
+                        </form>
+                    </div>
                 </div>
+
+                <p class="hidden">Below this is duplicates of the above code, when the page is generated it would loop and generate this per item</p>
+
                 <div class="card-Shop col-xl-3 col-lg-4 col-md-6 col-sm-12 col-xs-12">
-                    <img source=""></img>
+                    <img source="" alt="placeholder"></img>
                     <h1>Product Name</h1>
                     <p alt="product-Image">Description</p>
-                    <button type="button" action="">Add to cart</button>
-                    <input type="number"></input>
+                    <div class="flex grid-container">
+                        <form action="">
+                            <button type="submit" action="" class="col-md-8 col-xs-12">Add to cart</button>
+                            <input type="number" class="col-md-4 col-xs-12"></input>
+                            <input type="text" class="hidden" value="#ProductID"></input>
+                        </form>
+                    </div>
                 </div>
                 <div class="card-Shop col-xl-3 col-lg-4 col-md-6 col-sm-12 col-xs-12">
-                    <img source=""></img>
+                    <img source="" alt="placeholder"></img>
                     <h1>Product Name</h1>
                     <p alt="product-Image">Description</p>
-                    <button type="button" action="">Add to cart</button>
-                    <input type="number"></input>
+                    <div class="flex grid-container">
+                        <form action="">
+                            <button type="submit" action="" class="col-md-8 col-xs-12">Add to cart</button>
+                            <input type="number" class="col-md-4 col-xs-12"></input>
+                            <input type="text" class="hidden" value="#ProductID"></input>
+                        </form>
+                    </div>
                 </div>
                 <div class="card-Shop col-xl-3 col-lg-4 col-md-6 col-sm-12 col-xs-12">
-                    <img source=""></img>
+                    <img source="" alt="placeholder"></img>
                     <h1>Product Name</h1>
                     <p alt="product-Image">Description</p>
-                    <button type="button" action="">Add to cart</button>
-                    <input type="number"></input>
+                    <div class="flex grid-container">
+                        <form action="">
+                            <button type="submit" action="" class="col-md-8 col-xs-12">Add to cart</button>
+                            <input type="number" class="col-md-4 col-xs-12"></input>
+                            <input type="text" class="hidden" value="#ProductID"></input>
+                        </form>
+                    </div>
                 </div>
                 <div class="card-Shop col-xl-3 col-lg-4 col-md-6 col-sm-12 col-xs-12">
-                    <img source=""></img>
+                    <img source="" alt="placeholder"></img>
                     <h1>Product Name</h1>
                     <p alt="product-Image">Description</p>
-                    <button type="button" action="">Add to cart</button>
-                    <input type="number"></input>
+                    <div class="flex grid-container">
+                        <form action="">
+                            <button type="submit" action="" class="col-md-8 col-xs-12">Add to cart</button>
+                            <input type="number" class="col-md-4 col-xs-12"></input>
+                            <input type="text" class="hidden" value="#ProductID"></input>
+                        </form>
+                    </div>
                 </div>
+
+                
             </div>
         </div>
     )
