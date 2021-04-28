1898de3ed18f0331b7c8ec675d216534994b027f
jwstewart-7 <jwstewart-7@github.com>
2021-04-27 21:31:48 -0400

    Adjusted error code and removed some commented code

--- client/src/views/ShopPage/Sections/ShopGrid.js
+++ client/src/views/ShopPage/Sections/ShopGrid.js
@@ -5,9 +5,7 @@ import ShopCard from "./ShopCard.js";
 export default function Shop_Grid({products}) {
     if (products === undefined || products.length <= 0){
         return(
-            <div id="divShop" className="flex grid-container">
-                <h1 className="error">Error: Unable to load products!</h1>
-            </div>
+            <p className="hidden">This is a stub for if error checks are needed here</p>
         )
     }
 
