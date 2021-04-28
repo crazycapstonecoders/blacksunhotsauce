010a93cf93d112f22d3420f7dae4a4684d720343
jwstewart-7 <jwstewart-7@github.com>
2021-04-27 21:37:00 -0400

    implemented the error code suggestion and removed some comments

--- client/src/views/ShopPage/ShopPage.js
+++ client/src/views/ShopPage/ShopPage.js
@@ -21,12 +21,21 @@ export default function ShopPage() {
         })
     }
     
+    const showError = () => {
+        return error && (
+            <div id="divShop" className="flex grid-container">
+                <h1 className="error">Error: Unable to load products!</h1>
+            </div>
+        )
+    }
+    
     useEffect(() => {
         listProducts()
     }, [])
 
     return (
         <div>
+            {showError()}
             <Header
                 color="transparent"
                 brand="#testing"
@@ -43,86 +52,3 @@ export default function ShopPage() {
         </div>
     )
 }
-// import React, { useState, useEffect } from 'react'
-// import ReusableCard from '../../helpers/ReusableCard'
-// import IdleWarning from '../../helpers/IdleWarning' 
-// import { getProducts } from '../../api/coreApi' 
-// // nodejs library that concatenates classes
-// import classNames from "classnames";
-// // @material-ui/core components
-// import { makeStyles } from "@material-ui/core/styles";
-// // core components
-// import Header from "components/Header/Header.js";
-// import HeaderLinks from "components/Header/HeaderLinks.js";
-// import Parallax from "components/Parallax/Parallax.js";
-// import GridContainer from "components/Grid/GridContainer.js";
-// import GridItem from "components/Grid/GridItem.js";
-// import SnackbarContent from "components/Snackbar/SnackbarContent.js"
-
-// import styles from "assets/jss/material-kit-react/views/shopPage.js";
-
-// import '../ShopPage.scss'
-
-// const useStyles = makeStyles(styles)
-
-// export default function ShopPage() {
-//     const classes = useStyles()
-//     const [products, setProducts] = useState([])
-//     const [error, setError] = useState(false)
-
-//     const listProducts = () => {
-//         getProducts().then(res => {
-//             setProducts(res.data)
-//         }).catch(error => {
-//             setError(error)
-//         })
-//     }
-
-//     useEffect(() => {
-//         listProducts()
-//     }, [])
-
-//     const showError = () => {
-//         return error && (
-//             <SnackbarContent
-//             message={
-//                 <span>
-//                 {error}
-//                 </span>
-//             }
-//             close
-//             color="danger"
-//             icon="info_outline"
-//             />
-//         )
-//     }
-    
-//     return (
-//         <div>
-//             <Header
-//                 color="transparent"
-//                 brand="#testing"
-//                 rightLinks={<HeaderLinks />}
-//                 fixed
-//                 changeColorOnScroll={{
-//                 height: 400,
-//                 color: "white"
-//                 }}
-//             />
-//             <Parallax small filter image={require("assets/img/parallax.png")} />
-//             <IdleWarning />
-//             <div className={classNames(classes.main, classes.mainRaised)}>
-//                 {showError()}
-//                 <div className={classes.container}>
-//                     <GridContainer spacing={4}>
-//                         {products.map((product, i) => (
-//                             <GridItem key={i} xs={12} sm={6} md={4}>
-//                                 <ReusableCard product={product} showAddToCartButton={ product.quantity > 0 ? true : false } />
-//                             </GridItem> 
-//                         ))}
-//                     </GridContainer>
-//                 </div>
-//             </div>
-//         </div>
-//     )
-// }
